58c053783e975a15ca673211cc05c84cd5cd91e0
tanabe <tanablog@gmail.com>
2012-05-12 20:07:20 +0900

    Remove canvas tags.

--- public/js/star_graph.js
+++ public/js/star_graph.js
@@ -1,34 +1,55 @@
 function render_graph(data){
-    var canvas = document.getElementById('star_graph');
+    var GRAPH_HEIGHT = 60;
+    var GRAPH_WIDTH = 600;
+    var COLORS = ["#5BB75B", "#0074CC", "#49AFCD", "#FAA732", "#DA4F49"];
+    var graphElement = $("#evaluation_graph");
+    var totalScore = 0;
 
-    if(canvas.getContext){
-        var context = canvas.getContext('2d');
-        data_to_graph(context, data);
-    }
-}
+    var render = function() {
+        for (var i = 0; i < data.length; i++) {
+            totalScore += parseInt(data[i].value, 10);
+        }
 
-function data_to_graph(context, data){  
-    var colors = ["#5BB75B", "#0074CC", "#49AFCD", "#FAA732", "#DA4F49"];
-    var graph_width = 600;
-    var graph_height = 60;
-    var y_offset = 0;
+        for (var i = 0; i < data.length; i++) {
+            var cell = $("<div>");
+            cell.css({
+                "background-color": COLORS[i],
+                "display": "inline-block",
+                "width": Math.floor(GRAPH_WIDTH * data[i].value / totalScore) + "px",
+                "height": "60px"
+            });
+            graphElement.append(cell);
+        }
+    };
 
-    var offset = 0;
-    for(var i = 0; i < data.length; i++){
-        data[i].color = colors[i];
-        data[i].startValue = offset;
-        offset += data[i].value;
-    }
+    var render_caption = function() {
+        for (var i = 0; i < data.length; i++) {
+           var caption = $("<div>");
+            caption.css({
+                "display": "inline-block"
+            });
 
-    // 幅、x座標、y座標の取得
-    data.forEach(function(obj){
-        obj._width = obj.value / offset * graph_width;
-        obj._x = obj.startValue / offset * graph_width;
-        obj._y = y_offset;
-    });
+            var rect = $("<span>");
+            rect.css({
+                "background-color": COLORS[i],
+                "display": "inline-block",
+                "width": "10px",
+                "height": "10px"
+            });
+            caption.append(rect);
+            caption.append(":");
 
-    data.forEach(function(obj){
-        context.fillStyle = obj.color;
-        context.fillRect(obj._x, obj._y, obj._width, graph_height);
-    });
+            for (var j = data.length; j > i; j--) {
+                caption.append('&#9733;');
+            }
+
+            if (i < data.length - 1) {
+                caption.append('&nbsp;/&nbsp;');
+            }
+            $("#evaluation_graph_caption").append(caption);
+        }
+    };
+
+    render();
+    render_caption();
 }
